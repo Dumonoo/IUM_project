57217c7c9436d917b0cfca19415f09b9fe9aab0c
Dumono <domino200@vp.pl>
2022-11-30 00:14:49 -0800

    Basic missing values raports

---
+++ analyse_data/analyse.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from os import listdir
+from os.path import isfile
+
+import pandas as pd
+from IPython.display import display
+from IPython.display import HTML
+
+
+
+import json
+data_path = "data/"
+supported_extension = ".jsonl"
+raports_dir = "raports/"
+
+data_files = [file_name for file_name in listdir(data_path) if isfile(data_path + file_name)]
+data_jsonl = [file_name for file_name in data_files if file_name.endswith(supported_extension)]
+
+atributes = {}
+atributes_stats = []
+
+for file in data_jsonl:
+    file_loaded = pd.read_json(data_path+file, lines=True)
+    data_frame = file_loaded
+    print(f"Analyzing file {file}")
+    print(data_frame.info())
+    print("Null values info:")
+    print(data_frame.isnull().sum())
+    if file == "artists.jsonl":
+        missing_artists_id = data_frame.loc[data_frame["id"] == -1]
+        missing_artists_id.to_json(raports_dir+'missing_artists_id.jsonl', orient='records', lines=True)
+        artists_without_genres = data_frame.loc[data_frame["genres"].isnull()]
+        artists_without_genres.to_json(raports_dir+'artists_without_genres.jsonl', orient='records', lines=True)
+    elif file == "users.json":
+        pass
+    elif file == "tracks.jsonl":
+        missing_tracks_id = data_frame.loc[data_frame["id"].isnull()]
+        missing_tracks_id.to_json(raports_dir+'missing_tracks_id.jsonl', orient='records', lines=True)
+        missing_artists_for_track = data_frame.loc[data_frame["id_artist"].isnull()]
+        missing_artists_for_track.to_json(raports_dir+'missing_artists_for_track.jsonl', orient='records', lines=True)
+        missing_title_for_track = data_frame.loc[data_frame["name"].isnull()]
+        missing_title_for_track.to_json(raports_dir+'missing_artists_for_track.jsonl', orient='records', lines=True)
+    elif file == "sessions.jsonl":
+        sessions_without_user = data_frame.loc[data_frame["user_id"].isnull()]
+        sessions_without_user.to_json(raports_dir+'sessions_without_user.jsonl', orient='records', lines=True)
+        sessions_without_track = data_frame.loc[data_frame["event_type"] != "advertisment"].loc[data_frame["track_id"].isnull()]
+        sessions_without_track.to_json(raports_dir+'sessions_without_track.jsonl', orient='records', lines=True)
+        sessions_without_event_type = data_frame.loc[data_frame["event_type"].isnull()]
+        sessions_without_event_type.to_json(raports_dir+'sessions_without_event_type.jsonl', orient='records', lines=True)
+    else:
+        pass
--- jsonl_to_csv_converter/convert.py
+++ jsonl_to_csv_converter/convert.py
@@ -6,23 +6,26 @@ import json
 import csv
 # import unicodecsv as csv
 data_path = "data/"
-output_data_path = "output/"
+output_data_path = "jsonl_to_csv_converter/output_csv/"
 supported_extension = ".jsonl"
 output_extension = ".csv"
 data_files = [file_name for file_name in listdir(data_path) if isfile(data_path + file_name)]
 
 data_jsonl = [file_name for file_name in data_files if file_name.endswith(supported_extension)]
 
+# counter = 0
 for file in data_jsonl:
     with open(data_path + file, "r") as jsonl_file:
         json_list = list(jsonl_file)
+        
+        
 
     new_file_name = file[:-len(supported_extension)] + output_extension 
     output_data_file = open(output_data_path + new_file_name, "w", newline='', encoding='utf-8-sig')
     csv_writer = csv.writer(output_data_file, dialect='excel', delimiter=';')
 
     row_counter = 0
-
+# TODO fix correct headers
     for json_entry in json_list:
         row = json.loads(json_entry)
         # write header
