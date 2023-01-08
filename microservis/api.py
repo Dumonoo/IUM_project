61d4d51787541341e0c796bb1c52219148d85f29
Dumono <domino200@vp.pl>
2023-01-08 11:11:01 -0800

    Added basic main page

--- microservis/api.py
+++ microservis/api.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Request
+from fastapi.responses import HTMLResponse
 from datetime import datetime, time, timedelta
+
 from typing import Union
 
 app = FastAPI(title="App")
 
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+def root(request: Request):
+    html = f"<html><body><h1><a href='{request.url._url}docs'>Strona mikroserwisu</a></h1></body></html>"
+    return HTMLResponse(html)
 
 @app.get("/recommend_by_date/{advance_model}/{timestamp}/{user_id}")
 def get_recommendation_by_time_cutoff(user_id: int, timestamp: datetime , advance_model: bool = True):
