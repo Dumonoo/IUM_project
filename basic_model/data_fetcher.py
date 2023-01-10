5717f124f7f3745742f4f431cb36b58888fffdfa
Konstantin Panov <kpa@impicode.com>
2023-01-10 13:40:19 +0100

    Add KNearestNeighbots model

--- basic_model/data_fetcher.py
+++ basic_model/data_fetcher.py
@@ -1,26 +1,27 @@
 import pandas as pd
 from datetime import datetime
 
-track_columns = [
-    "popularity",
-    "duration_ms",
-    "explicit",
-    "danceability",
-    "energy",
-    "key",
-    "loudness",
-    "speechiness",
-    "acousticness",
-    "instrumentalness",
-    "liveness",
-    "valence",
-    "tempo",
-    "year",
-]
+# self.track_columns = [
+# "popularity",
+# "duration_ms",
+# "explicit",
+# "danceability",
+# "energy",
+# "key",
+# "loudness",
+# "speechiness",
+# "acousticness",
+# "instrumentalness",
+# "liveness",
+# "valence",
+# "tempo",
+# "year",
+# ]
 
 
 class DataFetcher:
-    def __init__(self) -> None:
+    def __init__(self, track_columns) -> None:
+        self.track_columns = track_columns
         self.tracks = pd.read_json("data/tracks.jsonl", lines=True)
         self.tracks["year"] = self.tracks.apply(
             lambda x: self.get_track_year(x["release_date"]), axis=1
@@ -30,13 +31,19 @@ class DataFetcher:
         self.sessions = pd.read_json("data/sessions.jsonl", lines=True)
         self.users = pd.read_json("data/users.jsonl", lines=True)
 
+    def get_songs(self, ids):
+        return self.tracks[self.tracks["id"].isin(ids)][self.track_columns].values
+
+    def get_by_index(self, i):
+        return self.tracks.iloc[i]
+
     def get_validation_data(self):
         return [
             self._get_validation_data_for_user(user) for user in self.users["user_id"]
         ]
 
     def get_training_data(self):
-        return self.tracks[track_columns].values
+        return self.tracks[self.track_columns].values
 
     def _get_validation_data_for_user(self, user):
         data = self.sessions
@@ -61,7 +68,7 @@ class DataFetcher:
         for id in ids:
             track_data = self.tracks[self.tracks["id"] == id]
             # Check if track exists in track_data
-            track_vector = track_data[track_columns].values
+            track_vector = track_data[self.track_columns].values
             tracks_vectors.append(track_vector)
         return tracks_vectors
 
