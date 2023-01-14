e8da3235c6c43f297baaa204abf788fc2f5e1590
Konstantin Panov <kpa@impicode.com>
2023-01-14 12:31:09 +0100

    change validation to measure precision

--- src/models/validation/validation.py
+++ src/models/validation/validation.py
@@ -11,26 +11,30 @@ data_fetcher = DataFetcher("all")
 
 def validate_model(model):
     model.train()
-    all_rates = []
+    all_tp = 0
+    all_fp = 0
     for user in range(101, 151):
-        rate = validate_for_user(model, user)
-        if rate is None:
+        tp, fp = validate_for_user(model, user)
+        if tp is None:
             continue
-        all_rates.append(rate)
-    if not len(all_rates):
+        all_tp += tp
+        all_fp += fp
+    if not (all_tp + all_fp):
         return 0
-    return sum(all_rates) / len(all_rates)
+    precision = all_tp / (all_tp + all_fp)
+    return precision
 
 
 def validate_for_user(model, user_id):
     all_liked = data_fetcher.get_all_liked_by_user(user_id)
-    all_listened_to = data_fetcher.get_all_listened_to_by_user(user_id)
     if len(all_liked) < 20:
-        return None
-    train = all_liked
-    test = all_listened_to
+        return None, None
+    train = all_liked[:10]
+    test = all_liked[10:]
     recommended = model.recommend(train)
-    return 1 if any([song in test for song in recommended]) else 0
+    true_positive = len([track for track in recommended if track in test])
+    false_positive = len([track for track in recommended if track not in test])
+    return true_positive, false_positive
 
 
 def validate_all_models():
