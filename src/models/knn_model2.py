b1b808e39baf735957d14f879240eac32f090f4a
Dumono <domino200@vp.pl>
2023-01-14 14:52:17 -0800

    More useless models

---
+++ src/models/knn_model2.py
@@ -0,0 +1,76 @@
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
+
+from src.models.utils.data_loader import DataLoader
+
+choosen_cols = [
+                "popularity",  # 0.06
+                "duration_ms",  # 0.53
+                "explicit",  # 0.02
+                "danceability",  # 0.44
+                "energy",  # 0.48
+                "key",  # 0.02
+                "loudness",  # 0.53
+                "speechiness",  # 0.48
+                "acousticness",  # 0.51
+                "instrumentalness",  # 0.46
+                "liveness",  # 0.53
+                "valence",  # 0.48
+                "tempo",  # 0.53
+                # "year",  # 0.04
+                #
+            ]
+
+class KNNModel:
+    def __init__(self) -> None:
+        self.data_obj = DataLoader()
+        self.tracks_data = self.data_obj.get_tracks()
+        self.scaler = StandardScaler()
+        self.model = NearestNeighbors(
+            n_neighbors=5, metric="cosine", algorithm="brute"
+        )
+        self.training_data = self.tracks_data[choosen_cols]
+        # print(self.tracks_data)
+    
+    def train(self):
+        X = self.training_data[choosen_cols].values
+        X = self.scaler.fit_transform(X)
+        self.model.fit(X)
+
+    def recommend(self, ids, user_id):
+        # print(ids)
+        input_songs_data = self.tracks_data[self.tracks_data['id'].isin(ids)][choosen_cols].values
+        input_indexes = self.data_obj.get_indexes_of_tracks(ids)
+        X = self.scaler.transform(input_songs_data)
+        distances, indices = self.model.kneighbors(X)
+        score_board = {}
+
+        for d, i in zip(distances, indices):
+            for x in range(len(d)):
+                if i[x]  in input_indexes:
+                    continue
+                if i[x] not in score_board:
+                    score_board[i[x]] = d[x]
+                else:
+                    score_board[i[x]] = min(d[x], score_board[i[x]])
+        recomended_index = [key for key, value in sorted(score_board.items(), key=lambda item: item[1])]
+        # recommended_id = self.data_obj.get_ids_of_given_ids(recomended_index[:10])
+        recommended_id = self.data_obj.get_ids_of_given_ids(recomended_index)
+
+        usr_fav_genres = self.data_obj.get_user_fav_genres(user_id)
+        genres_tracks = self.data_obj.get_tracks_of_genres(usr_fav_genres)
+
+        # Podaj max 8 utworow z ulubionych generes
+        genres_rec_ids = []
+        not_ids = []
+        for i in recommended_id:
+            if not genres_tracks.loc[genres_tracks['id'] == i].empty:
+                genres_rec_ids.append(i)
+            else:
+                not_ids.append(i)
+        
+        # recommended_id = genres_rec_ids + not_ids
+
+        # print("TTT", genres_tracks)
+
+        return recommended_id[:10]
--- src/models/random_model_genres.py
+++ src/models/random_model_genres.py
@@ -9,5 +9,9 @@ class RandomModelGenres:
     def recommend(self, ids, usr_id):
         usr_fav_genres = self.data_obj.get_user_fav_genres(usr_id)
         genres_tracks = self.data_obj.get_tracks_of_genres(usr_fav_genres)
-        # genres_tracks[~genres_tracks["id"].isin(ids)]
+        # genres_tracks = self.data_obj.get_tracks()
+        genres_tracks[~genres_tracks["id"].isin(ids)]
+        # print(genres_tracks.sample(10))
+        # return genres_tracks.sample(10, weights='popularity')["id"].values
         return genres_tracks.sample(10)["id"].values
+        # return genres_tracks.sample(10, "popularity")["id"].values
