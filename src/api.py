dc27ea2876804b293d5212075ade90e6cb03f9ec
Konstantin Panov <kpa@impicode.com>
2023-01-15 21:56:42 +0100

    tmp

--- src/api.py
+++ src/api.py
@@ -6,19 +6,72 @@ from datetime import datetime
 import pandas as pd
 from logger import log_info, log_error
 from models.validation.validation import validate_all_models
-from models.knn_model import KNNModel
-from models.random_model import RandomModel
 from models.validation.validation import validate_model
 from models.utils.get_user_data import analyse_user
 from models.utils.data_fetcher import DataFetcher
+from models.regression_recommender import RegressionRecommender, RandomRegression
+
 
 class ModelEnum(Enum):
     KNN = "KNN"
     Random = "Random"
 
+
 # Group A -> Model Random Group B -> Model KNN
-users_group_A = [116, 130, 124, 138, 101, 142, 148, 144, 135, 136, 137, 122, 143, 125, 145, 107, 147, 126, 140, 146, 105, 129, 128, 103, 106]
-users_group_B = [127, 115, 110, 109, 133, 119, 149, 108, 112, 114, 131, 113, 141, 118, 139, 111, 134, 117, 102, 123, 132, 104, 120, 121, 150]
+users_group_A = [
+    116,
+    130,
+    124,
+    138,
+    101,
+    142,
+    148,
+    144,
+    135,
+    136,
+    137,
+    122,
+    143,
+    125,
+    145,
+    107,
+    147,
+    126,
+    140,
+    146,
+    105,
+    129,
+    128,
+    103,
+    106,
+]
+users_group_B = [
+    127,
+    115,
+    110,
+    109,
+    133,
+    119,
+    149,
+    108,
+    112,
+    114,
+    131,
+    113,
+    141,
+    118,
+    139,
+    111,
+    134,
+    117,
+    102,
+    123,
+    132,
+    104,
+    120,
+    121,
+    150,
+]
 
 
 app = FastAPI(title="Recomenndation_API")
@@ -41,36 +94,40 @@ def root(request: Request):
     html = f"<html><body><h1><a href='{request.url._url}docs'>Strona docs dla mikroserwisu</a></h1></body></html>"
     return HTMLResponse(html)
 
+
 # Main endpoints
 # Recommend 10 tracks playlist for given user_id for today with selected model
-@app.get('/recommendation/{model_name}/{user_id}')
+@app.get("/recommendation/{model_name}/{user_id}")
 def get_recommendation(model_name: ModelEnum, user_id: int):
     # TODO change this to function with will return information ready for model
     ids, timestamp = analyse_user(user_id).get_n_last_liked(10)
     recommended = []
     if model_name is ModelEnum.KNN:
         recommended = target_model.recommend(ids)
-    
+
     if model_name is ModelEnum.Random:
         recommended = base_model.recommend(ids)
     else:
-        # Model Random 
+        # Model Random
         log_error(f"Uknown model {model_name.name}")
         recommended = base_model.recommend(ids)
 
     recommended_list = data_fetcher.get_songs(recommended).tolist()
-    log_info(user_id=user_id, recommended_tracks=recommended_list, model_name=model_name.name)
+    log_info(
+        user_id=user_id, recommended_tracks=recommended_list, model_name=model_name.name
+    )
     return recommended_list
 
+
 # Recommend 10 tracks playlist for given user_id for today
-@app.get('/ab_recommendation/{user_id}')
+@app.get("/ab_recommendation/{user_id}")
 def get_ab_recommendation(user_id: int):
     # TODO change this to function with will return information ready for model
     ids, timestamp = analyse_user(user_id).get_n_last_liked(10)
     recommended = []
     model_name = ""
     if user_id in users_group_A:
-        # Group A -> Model Random 
+        # Group A -> Model Random
         recommended = base_model.recommend(ids)
         model_name = ModelEnum.Random
     if user_id in users_group_B:
@@ -78,28 +135,39 @@ def get_ab_recommendation(user_id: int):
         recommended = target_model.recommend(ids)
         model_name = ModelEnum.KNN
     else:
-        # UNKOWN Group -> Model Random 
+        # UNKOWN Group -> Model Random
         log_error("The given user is not in any of the groups a and b", user_id=user_id)
         model_name = ModelEnum.Random
         recommended = base_model.recommend(ids)
 
     recommended_list = data_fetcher.get_songs(recommended).tolist()
-    log_info(user_id=user_id, recommended_tracks=recommended_list, model_name=model_name.name, is_AB=True)
-    return recommended_list    
+    log_info(
+        user_id=user_id,
+        recommended_tracks=recommended_list,
+        model_name=model_name.name,
+        is_AB=True,
+    )
+    return recommended_list
 
 
 # Additional endpoints for testing purposes
 # Recommend 10 tracks playlist for user_id with selected model using sessions before timestamp
-@app.get('/recommendation_for_timestamp/{model_name}/{timestamp}/{user_id}')
-def get_recommendation_for_timestamp(model_name: ModelEnum, user_id: int, timestamp: datetime):
+@app.get("/recommendation_for_timestamp/{model_name}/{timestamp}/{user_id}")
+def get_recommendation_for_timestamp(
+    model_name: ModelEnum, user_id: int, timestamp: datetime
+):
     ...
+
 
 # Recommend 10 tracks playlist for user_id with selected model using sessions before session_id
-@app.get('/recommendation_for_session_id/{model_name}/{session_id}/{user_id}')
-def get_recommendation_for_session_id(model_name: ModelEnum, user_id: int, session_id: int):
+@app.get("/recommendation_for_session_id/{model_name}/{session_id}/{user_id}")
+def get_recommendation_for_session_id(
+    model_name: ModelEnum, user_id: int, session_id: int
+):
     ...
 
+
 # Validate both all models and return infomation about them
-@app.get('/validate_models/')
+@app.get("/validate_models/")
 def validate_models():
     return validate_all_models()
--- src/run_validation.py
+++ src/run_validation.py
@@ -1,37 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from models.validation.validation import validate_all_models
 from models.utils.data_fetcher import DataFetcher
-from models.utils.data_loader import DataLoader
-from models.cosine_model import CosineModel
-from models.cf_model import CFModel
 from models.regression_recommender import RegressionRecommender, RandomRegression
-from models.random_model import RandomModel
-from models.class_knn_model import KNNClassifierModel
-import random
-
-
-def validate_knn_class():
-    fetcher = DataFetcher("all")
-    model = KNNClassifierModel()
-    user = 101
-    rated = fetcher.get_all_rated(user)
-    data = fetcher.get_training_data(user)
-    data = data.merge(rated, left_on="id", right_on="track_id")
-    train, test = train_test_split(data, test_size=0.5, shuffle=False)
-    model.train(train)
-    prediction = model.predict(test)
-    result = prediction.merge(test[["id", "event_type"]])
-    tp, fp, tn = 0, 0, 0
-    for id, prediction, label in result.values:
-        match prediction, label:
-            case "like", "like":
-                tp += 1
-            case "like", "skip":
-                fp += 1
-            case _, _:
-                ...
-    print(tp / (tp + fp))
 
 
 def validate(data, base=False):
@@ -54,54 +24,11 @@ def validate(data, base=False):
             len([t for p, t in pred_and_test[-10:] if p >= 0.75 and t >= 0.75])
             / len(list(pred_and_test[:10])),
         )
-    print("Base:" if base else "Model:")
-    print("MAE:", sum(all_errors) / len(all_errors))
-    print("BIS:", sum(bis) / len(bis))
+    return sum(bis) / len(bis)
 
 
 if __name__ == "__main__":
     fetcher = DataFetcher("all")
     data = {user: fetcher.read_avg_listen(user) for user in range(101, 151)}
-    validate(data, False)
-    validate(data, True)
-    # validate_regression()
-    # validation_random_regression()
-
-    # load = DataLoader()
-    # load.get_user_sessions_with_estimations(101)
-    # validate_knn_class()
-    # validate_regression()
-
-    # model.write()
-    # print(model.similarities)
-    # fetcher = DataFetcher("all")
-    # res = fetcher.read_avg_listen(101)
-    # print(res)
-    # model = CFModel()
-    # model.train()
-    # liked = fetcher.get_all_liked_by_user(101)
-    # skipped = fetcher.get_all_skipped_by_user(101)
-    # recs = model.recommened(101)
-    # tp = len([t for t in recs if t in liked])
-    # fp = len([t for t in recs if t in skipped])
-    # print(tp / (tp + fp))
-    # m = RegressionRecommender()
-    # m.train(101)
-
-    # print(validate_all_models())
-
-
-def validate_cosine():
-    model = CosineModel()
-    model.read()
-    tp, fp = 0, 0
-    for user in range(50, 151):
-        model.limit_to_user(user)
-        recs = model.recommend_to_user(user)
-        liked = model.fetcher.get_all_liked_by_user(user)
-        listened = model.fetcher.get_all_listened_to_by_user(user)
-        skipped = model.fetcher.get_all_skipped_by_user(user)
-        tp += len([t for t in recs if t in listened])
-        fp += len([t for t in recs if t in skipped])
-        model.unlimit()
-    print(tp / (tp + fp))
+    print('Docelowe:', validate(data, False))
+    print('Bazowe:', validate(data, True))
